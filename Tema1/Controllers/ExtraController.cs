e2d5f72475e53c05124e8295609da47308519c14
Ovidiu <Ovidiu@DESKTOP-CRSSCUC>
2018-12-04 08:59:07 +0200

    Extra Done: Contine"Fun" cu HTML Basic Table

---
+++ Tema1/Controllers/ExtraController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tema1.Controllers
+{
+    public class ExtraController : Controller
+    {
+        // GET: Extra
+        public ActionResult Fun()
+        {
+            return View();
+        }
+    }
+}
