b049e2a51451b13ac626ca9842161086d7cbcedb
Ovidiu <Ovidiu@DESKTOP-CRSSCUC>
2018-12-10 16:15:17 +0200

    some variables and conditions

---
+++ Tema1/Controllers/NewController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tema1.Controllers
+{
+    public class NewController : Controller
+    {
+        // GET: New
+        public ActionResult RDR2()
+        {
+          
+
+            return View();
+        }
+    }
+}
